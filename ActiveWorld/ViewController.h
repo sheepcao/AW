91446240d87afa912b1d0e135822e3e4765e7f40
Eric Cao <ericcao@ML00070057.local>
2014-10-10 22:39:18 +0800

    first try

--- ActiveWorld/ViewController.h
+++ ActiveWorld/ViewController.h
@@ -7,9 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "gameLevelController.h"
+#define MaxSport 2
+#define MaxCommunity 2
+#define MaxOutdoor 2
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIScrollView *SportMenu;
 
+@property (weak, nonatomic) IBOutlet UIScrollView *CommunityMenu;
+@property (weak, nonatomic) IBOutlet UIScrollView *OutdoorMenu;
+
+@property (strong ,nonatomic) gameLevelController *game;
 @end
 
--- ActiveWorld/ViewController.m
+++ ActiveWorld/ViewController.m
@@ -14,11 +14,58 @@
 
 @implementation ViewController
 
+@synthesize SportMenu;
+@synthesize CommunityMenu;
+@synthesize OutdoorMenu;
+@synthesize game;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    game = [[gameLevelController alloc] initWithNibName:@"gameLevelController" bundle:nil];
 }
 
+-(void)viewDidAppear:(BOOL)animated
+{
+    int level = 1;
+    level = [self initScorll:SportMenu withSeed:level withMax:MaxSport];
+    level = [self initScorll:CommunityMenu withSeed:level withMax:MaxSport + MaxCommunity];
+    level = [self initScorll:OutdoorMenu withSeed:level withMax:MaxSport + MaxCommunity + MaxOutdoor];
+
+}
+
+-(int)initScorll:(UIScrollView *)myScroll withSeed:(int)seed withMax:(int)levelMax;
+{
+    int position = 0;
+    
+    myScroll.contentSize =CGSizeMake(myScroll.frame.size.width, myScroll.frame.size.height*1.5);
+    myScroll.contentOffset = CGPointMake(0, -myScroll.frame.size.height/4);
+    myScroll.contentInset = UIEdgeInsetsMake(2,0,0,0);
+//    NSLog(@"scroll:%@",myScroll);
+    for (int i = seed; i <= levelMax; i++)
+    {
+        
+        UIButton *contentButton = [[UIButton alloc] initWithFrame:CGRectMake(5, 5+position * myScroll.frame.size.width, myScroll.frame.size.width-10, myScroll.frame.size.width-10)];
+        contentButton.tag = i;
+        [contentButton setImage:[UIImage imageNamed:[NSString stringWithFormat:@"pic%d",i]] forState:UIControlStateNormal];
+        [contentButton addTarget:self action:@selector(chooseLevel:) forControlEvents:UIControlEventTouchUpInside];
+        seed = i;
+        [myScroll addSubview:contentButton];
+        position++;
+    }
+    seed++;
+    return seed;
+}
+
+-(void)chooseLevel:(UIButton *)button
+{
+    NSLog(@"button:%ld",button.tag);
+    game.levelNumber = [NSNumber numberWithInteger:button.tag];
+    [self.navigationController pushViewController:game animated:YES];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
