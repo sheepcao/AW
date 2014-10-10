7e8721192cb7c1a6c234fe49f4f7cebd9c5855f6
Eric Cao <ericcao@ML00070057.local>
2014-10-10 14:59:25 +0800

    Initial Commit

---
+++ ActiveWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ActiveWorld
+//
+//  Created by Eric Cao on 10/10/14.
+//  Copyright (c) 2014 Eric Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ActiveWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActiveWorld
+//
+//  Created by Eric Cao on 10/10/14.
+//  Copyright (c) 2014 Eric Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
